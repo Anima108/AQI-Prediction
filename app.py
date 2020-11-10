be2836d966e394a333afda09ebe205956998c0fc
akankshatanwar1701 <akankshatanwar73@gmail.com>
2020-11-10 21:30:27 +0530

    Corrected app

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ import numpy as np
 # In[4]:
 
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='.')
 
 
 # In[5]:
@@ -35,41 +35,52 @@ model=pickle.load(open('model_svm','rb'))
 
 
 @app.route('/',methods=['GET'])
-def hello_world():
-    if flask.request.method == 'GET':
-        return render_template('AQI.html')
+def main():
+        return render_template("AQI.html")
 
 # In[ ]:
 
 # In[7]:
 
-@app.route('/predict', methods=['POST'])
-
+@app.route('/', methods=['POST'])
 def predict():
     #print(request.form)
-    int_features=[str(x) for x in request.form.values()]
-    req_model=int_features[12]
+    if request.method=='POST':
 
-    for x in range (11):
-        final[x]=float(int_features[x])
-    #final=np.array(features)
-    #print(features)
-    #print(final)
-    
-    arr=final.reshape(1, -1)
+        final=[]
 
-    prediction=model.predict(arr)
+        final.append(float(request.form['pm2']))
+        final.append(float(request.form['pm10']))
+        final.append(float(request.form['no']))
+        final.append(float(request.form['no2']))
+        final.append(float(request.form['nox']))
+        final.append(float(request.form['nh3']))
+        final.append(float(request.form['co']))
+        final.append(float(request.form['so2']))
+        final.append(float(request.form['o3']))
+        final.append(float(request.form['benzene']))
+        final.append(float(request.form['toluene']))
+        final.append(float(request.form['xylene']))
+        
+        final=np.array(final)
+        
+        #print(features)
+        #print(final)
+        
+        arr=final.reshape(1, -1)
+
+        prediction=model.predict(arr)
+        
+        #output='{0:.{1}f}'.format(prediction[0][1], 2)
     
-    output='{0:.{1}f}'.format(prediction[0][1], 2)
-    
-    return render_template('AQI.html',pred = 'Pedicted AQI - {}'.format(output))
+    return render_template("AQI.html",pred = prediction)
 
 
 # In[ ]:
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 
 # In[ ]:
