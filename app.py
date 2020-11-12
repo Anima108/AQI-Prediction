7b652173d64cb4cb56376fbb28306f2f9103a7b2
[Anima jain] <[anima.jain18@gmail.com]>
2020-11-12 14:37:45 +0530

    app.py corrected

--- app.py
+++ app.py
@@ -37,12 +37,9 @@ def hypothesis(x,theta):
 svm_model=pickle.load(open('model_svm','rb'))
 mlp_model=pickle.load(open('model_mlp','rb'))
 lr_model=pickle.load(open('model_lr','rb'))
-<<<<<<< HEAD
-
-=======
 rf_model=pickle.load(open('model_rf','rb'))
 dt_model=pickle.load(open('model_dt','rb'))
->>>>>>> 6185b42758c6b66ef029d2ef77ff3761e4357832
+
 
 # In[6]:
 
@@ -60,13 +57,10 @@ def predict():
     #print(request.form)
     if request.method == 'POST':
 
-<<<<<<< HEAD
+
         final=[]
         final_lr=[]
-=======
-        final = []
-        final_lr = []
->>>>>>> 6185b42758c6b66ef029d2ef77ff3761e4357832
+
 
         final.append(float(request.form['pm2']))
         final.append(float(request.form['pm10']))
@@ -80,42 +74,8 @@ def predict():
         final.append(float(request.form['benzene']))
         final.append(float(request.form['toluene']))
         final.append(float(request.form['xylene']))
-<<<<<<< HEAD
-        req_model=str(request.form['model'])
         
-        if req_model == "Support Vector Regression":
-            final=np.array(final)
-            arr=final.reshape(1, -1)
-            prediction=svm_model.predict(arr)
 
-        if req_model == "Multi-Layer Perceptron":
-            final=np.array(final)
-            arr=final.reshape(1, -1)
-            prediction=mlp_model.predict(arr)
-
-        if req_model == "Linear Regression":
-            theta,error_list= lr_model
-            final_lr.append(1)
-            for x in range(0,len(final)):
-                final_lr.append(final[x])
-            final_lr=np.array(final_lr)
-            prediction = hypothesis(final_lr,theta)
-
-
-    prediction = round(prediction,2)
-    if prediction<=50:
-        return render_template("AQI.html",pred ='Minimal Impact. \n AQI is {}'.format(prediction))
-    if prediction>=51 and prediction<=100:
-        return render_template("AQI.html",pred ='Minor breathing discomfort to sensitive people. \n AQI is {}'.format(prediction))
-    if prediction>=101 and prediction<=200:
-        return render_template("AQI.html",pred ='Breathing discomfort to the people with lungs, asthma and heart disease. \n AQI is {}'.format(prediction))
-    if prediction>=201 and prediction<=300:
-        return render_template("AQI.html",pred ='Breathing discomfort to most people on prolonged exposure. \n AQI is {}'.format(prediction))
-    if prediction>=301 and prediction<=400:
-        return render_template("AQI.html",pred ='Respiratory illness on prolonged exposure. \n AQI is {}'.format(prediction))  
-    if prediction>=401:
-        return render_template("AQI.html",pred ='Affects healthy people and seriously impacts those with the existing disease. \n AQI is {}'.format(prediction))  
-=======
         req_model = str(request.form['model'])
         
         if req_model == "Support Vector Regression":
@@ -165,7 +125,7 @@ def predict():
         return render_template("AQI.html",pred ='AQI is {}: \nVery Poor; \nRespiratory illness on prolonged exposure.'.format(prediction))  
     if prediction>=401:
         return render_template("AQI.html",pred ='AQI is {}: \nSevere; \nAffects healthy people and seriously impacts those with the existing disease.'.format(prediction))  
->>>>>>> 6185b42758c6b66ef029d2ef77ff3761e4357832
+
 
 
 # In[ ]:
